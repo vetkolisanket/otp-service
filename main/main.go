d5717e79168d32a26e8bd49dcb5e8f6caaa1564e
Sanket <vetkolisanket@gmail.com>
2019-02-27 11:15:55 +0530

    Using the newly created handler to serve requests

--- main/main.go
+++ main/main.go
@@ -10,16 +10,18 @@ import (
 func main() {
 	server := &http.Server{
 		Addr:              ":1234",
-		Handler:           nil,
+		Handler:           NewHTTPHandler(),
 		ReadHeaderTimeout: 2 * time.Second,
 		WriteTimeout:      1 * time.Minute,
 	}
 
+	log.Println("otp-service is up")
+
 	log.Fatal(server.ListenAndServe())
 }
 
-//NewHTTP handles routing of api requests for otp-service
-func NewHTTP() http.HandlerFunc {
+//NewHTTPHandler provides handler for routing of api requests for otp-service
+func NewHTTPHandler() http.HandlerFunc {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/otp-service/ping", func(w http.ResponseWriter, r *http.Request) {
