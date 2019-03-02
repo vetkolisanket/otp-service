b99ff6db85b26c74ac615371106f0757a15a7ced
Sanket <vetkolisanket@gmail.com>
2019-03-02 18:27:41 +0530

    Added a redis client

--- main/main.go
+++ main/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-redis/redis"
 )
 
 func main() {
@@ -24,9 +26,23 @@ func main() {
 func NewHTTPHandler() http.HandlerFunc {
 	mux := http.NewServeMux()
 
+	RedisNewClient()
+
 	mux.HandleFunc("/otp-service/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("content-type", "application/json")
 		fmt.Fprintf(w, `{"success":true}`)
 	})
 	return mux.ServeHTTP
+}
+
+//RedisNewClient ...
+func RedisNewClient() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
 }
